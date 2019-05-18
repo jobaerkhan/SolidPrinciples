33ec34526ee68ca6fc89ac49d2628cf3efe8fa25
jobaer <aan_jobaer@outlook.com>
2019-05-18 20:36:51 +0600

    Project Added

---
+++ SolidPrinciples.DIP/Database.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolidPrinciples.DIP
+{
+    public class Database : IDatabase
+    {
+        public void Add() { }
+    }
+
+    public interface IDatabase
+    {
+        void Add();
+    }
+}
---
+++ SolidPrinciples.ISP/Solution/CashOnDeliveryOrderProcess.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolidPrinciples.ISP.Solution
+{
+    //Separate ValidateCardInfo for Online Process Order so that CashOnDeliveryOrderProcess doesn't have to worry about Card Payment
+    class CashOnDeliveryOrderProcess : IOrderProcess
+    {
+        public void ProcessOder()
+        {
+            // APPLICABLE
+        }
+
+        public bool ValidateShippingAddress()
+        {
+            // APPLICABLE
+            return true;
+        }
+    }
+}
